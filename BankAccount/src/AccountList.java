e272a2eb3b7d277bc69cb2046f8ddca7792876da
Nookie25 <43532108+Nookie25@users.noreply.github.com>
2019-03-06 10:28:16 +0800

    Add files via upload

--- BankAccount/src/AccountList.java
+++ BankAccount/src/AccountList.java
@@ -1,6 +1,6 @@
 //Bank Account Linked List and Available Methods
 import java.io.Serializable;
-import java.util.Collections;
+import java.text.DecimalFormat;
 public class AccountList implements Serializable{
 
 protected ListNode head;
@@ -9,17 +9,27 @@ protected ListNode tail;
 
 		  
 		  int ListSize = 0;
-	
+		  DecimalFormat Billion = new DecimalFormat("##0.####");
 
-
+	/*
+	 * Constructor
+	 */
 	public AccountList() {
 		head = tail = null;
 	}
-
+	/*
+	 * @method GetListSize - this gets the number of accounts created
+	 * @param none
+	 * @returns ListSize - the number of accounts in the list
+	 */
 	public int GetListSize() {
 		return ListSize;
 	}
-	
+	/*
+	 * @method isEmpty -  This method checks if the list is Empty
+	 * @param none
+	 * @returns true if list isEmpty
+	 */
 	public boolean isEmpty() {
 		if(head==null) {
 			return true;
@@ -27,6 +37,12 @@ protected ListNode tail;
 			return false;
 	}
 	
+	/*
+	 * @method Deposit - this method enables the user to add money to his/her account
+	 * @param accnum - the account number input by the user
+	 * @param value - the desired number of money to deposit by the user
+	 * @return value - the amount deposited by the user
+	 */
 	public float Deposit (int accnum, float value) {
 		Account fromACC = null;
 		ListNode temp;
@@ -37,7 +53,12 @@ protected ListNode tail;
 			fromACC.Balance = fromACC.Balance + value;
 			return value;
 	}
-	
+	/*
+	 * @method Withdraw - this allows the user to get money from his/her account
+	 * @param accnum - account number input by the user
+	 * @param value - the desired amount to be withdrawn by the user
+	 * @return value - the amount withdrawn  
+	 */
 	public float Withdraw (int accnum, float value) {
 		Account fromACC = null;
 		ListNode temp;
@@ -48,14 +69,22 @@ protected ListNode tail;
 			fromACC.Balance = fromACC.Balance - value;
 			return value;
 	}
-	
+	/*
+	 * @method GetBalance - the method allows the user to input his/her account number then outputs the account's total balance
+	 * @param accnum - account number input by the user
+	 * @return temp2.info.Balance - the total balance of the user 
+	 */
 	public float GetBalance(int accnum) {
 		ListNode temp2;
 		for (temp2 = head; temp2 != null && temp2.info.AccNum != accnum; temp2 = temp2.next);
 		return temp2.info.Balance;
 	}
 	
-	//creates an int array to store the ID contents of the Account linked list
+	/*
+	 * @method CreateIntArray - creates an int array to store the ID contents of the Account linked list
+	 * @param 
+	 * @return SortedArrayID - the sorted account numbers in ascending order
+	 */
 	public int [] CreateIntArray() {
 		int [] array;
 		array = new int[ListSize];
@@ -64,7 +93,11 @@ protected ListNode tail;
 		return SortedArrayID;
 	}
 	
-	//transfers the linked list to an array and sorts it according to ID number directly
+	/*
+	 * @method PrintSortInt - transfers the linked list to an array and sorts it according to ID number directly using insertion sort
+	 * @param array - array with the linked list data
+	 * @return array - a list of account numbers in order
+	 */
 	public int [] PrintSortInt(int [] array) {
 		int i = 0;
 		for (ListNode temp = head  ; temp != null ; temp = temp.next) {
@@ -87,7 +120,11 @@ protected ListNode tail;
         return array;
     } 
 	
-    //insertion sort for sorting Account by Name (A - Z)
+    /*
+     * @method CreateNameSortArray - bubble sort for sorting Account by Name (A - Z)
+     * @param 
+     * @return SortedArrayAccount - a list of names in order
+     */
 	public Account[] CreateNameSortArray() {
 		Account [] array = new Account [ListSize];
 		Account [] SortedArrayAccount = new Account [ListSize];
@@ -105,27 +142,35 @@ protected ListNode tail;
 		}
 		return SortedArrayAccount;
 	}
-    //insertion sort for sorting Account by Balance (Low to High)
+    /*
+     * @method CreateBalanceSortArray - insertion sort for sorting Account by Balance (Low to High)
+     * @param 
+     * @return SortedArrayAccount - the accounts in ascending order according to balance
+     */
 	public Account [] CreateBalanceSortArray() {
 		Account [] array = new Account[ListSize];
 		Account [] SortedArrayAccount = new Account [ListSize];
 		SortedArrayAccount = ListToArray(array);
-		for(int i = 1;  i < SortedArrayAccount.length; i++) {
-			Account temp;
-			for (int j = i; j > 0; j--) {
-				if ((SortedArrayAccount[j].Balance) < (SortedArrayAccount[j-1].Balance))
-	            {
-	                temp = SortedArrayAccount[j];
-	                SortedArrayAccount[j] = SortedArrayAccount[j - 1];
-	                SortedArrayAccount[j - 1] = temp;
-	            }
-	        }
-		}
+		int n = array.length;
+		 for (int i=1; i<n; ++i) { 
+	            Account key = SortedArrayAccount[i]; 
+	            int j = i-1; 
+	            while (j>=0 && SortedArrayAccount[j].Balance > key.Balance) 
+	            { 
+	                SortedArrayAccount[j+1] = SortedArrayAccount[j]; 
+	                j = j-1; 
+	            } 
+	            SortedArrayAccount[j+1] = key; 
+	        } 
+	        return SortedArrayAccount;
+	    } 
 		
-		return SortedArrayAccount;
-	}
 	
-	//transfer the linked list to an array
+	/*
+	 * @method ListToArray - transfer the linked list to an array
+	 * @param array - the array that will receive the information of the linked list
+	 * @return array - the array that is filled with the data of the linked list
+	 */
 	public Account[] ListToArray(Account [] array) {
 		int i = 0;
 		for (ListNode temp = head  ; temp != null ; temp = temp.next) {
@@ -136,13 +181,22 @@ protected ListNode tail;
 	}
 	
 	
-   //Gets the Name of the entered account number
+   /*
+    * @method GetName - Gets the Name of the entered account number
+    * @param accnum - account number input by the user
+    * @return temp2.info.Name - name of the account number 
+    */
 	public String GetName(int accnum) {
 		ListNode temp2;
 		for (temp2 = head; temp2 != null && temp2.info.AccNum != accnum; temp2 = temp2.next);
 		return temp2.info.Name;
 	}
 	
+	/*
+	 * @method AddAccount - enables the user to add an account
+	 * @param add - the account to be added
+	 * @return void
+	 */
 	public void AddAccount(Account add) {
 		if (!isEmpty()){
 			tail.next = new ListNode(add);
@@ -151,6 +205,11 @@ protected ListNode tail;
 		ListSize++;
 	}
 	
+	/*
+	 * @method DeleteAccount - enables the user to remove a created account
+	 * @param accnum - account number input by the user
+	 * @return del - returns the deleted account
+	 */
 	public Account DeleteAccount(int accnum) {
 		Account del = null;
 		ListNode temp;
@@ -176,21 +235,35 @@ protected ListNode tail;
 			ListSize--;
 		return del;
 	}
-	
+	/*
+	 * @method InList - helps the user find an account number in the list
+	 * @param accnum - account number input by the user
+	 * @return temp2 - if the input account number by the user has a match in the list, temp2 returns true otherwise, false.
+	 */
 	public boolean InList(int accnum) {
 		ListNode temp2;
 		for (temp2 = head; temp2 != null && temp2.info.AccNum != accnum; temp2 = temp2.next);
 		return temp2 != null;
 	}
-	
+	/*
+	 * @method account PrintInLst - this method inputs a number and outputs the information of the object Account
+	 * @param int accnum - is the account number sent by the user
+	 * @returns temp2.info - prints the information of the specified account number
+	 */
 	public Account PrintInList(int accnum) {
 		ListNode temp2;
 		for (temp2 = head; temp2 != null && temp2.info.AccNum != accnum; temp2 = temp2.next);
-		System.out.printf( "Name: " +"\t"+ temp2.info.Name  +"\t\t"+  "Account Number: " +"\t"+ temp2.info.AccNum +"\t"+ " Balance: " +"\t"+ temp2.info.Balance);
+		System.out.printf( "Name: " +"\t"+ temp2.info.Name  +"\t\t"+  "Account Number: " +"\t"+ temp2.info.AccNum +"\t"+ " Balance: " +"\t"+ Billion.format(temp2.info.Balance));
 		System.out.println();
 		return temp2.info;
 	}
 	
+	/*
+	 * @method Transfer - this method enables the user to transfer transactions in between two accounts
+	 * @param from - account number of the source account
+	 * @param to - account number of the destination account 
+	 * @returns void
+	 */
 	public void Transfer(int from, int to, float amount) {
 		Account fromACC = null;
 		Account toACC = null;
@@ -209,10 +282,14 @@ protected ListNode tail;
 			fromACC.Balance = fromACC.Balance - amount;
 			toACC.Balance = toACC.Balance + amount;
 	}
-	
+	/*
+	 *@method PrintAll - this method prints all accounts according to chronological order of creation
+	 *@param none
+	 *@returns void
+	 */
 	public void PrintAll() {
 		for (ListNode temp = head; temp != null; temp = temp.next) {
-			System.out.printf( "Name: " +"\t"+ temp.info.Name  +"\t\t"+  "Account Number: " +"\t"+ temp.info.AccNum +"\t"+ " Balance: " +"\t"+ temp.info.Balance);
+			System.out.printf( "Name: " +"\t"+ temp.info.Name  +"\t\t"+  "Account Number: " +"\t"+ temp.info.AccNum +"\t"+ " Balance: " +"\t"+ Billion.format(temp.info.Balance));
 			System.out.println();
 		}
 			
